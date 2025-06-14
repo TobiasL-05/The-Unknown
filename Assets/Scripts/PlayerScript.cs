5d08922e9ce784a767e60bcf61ca5d616cf1027c
ThatTygo <tygomarctenwolde@gmail.com>
2025-06-14 22:08:33 +0200

    Kloten/testen/tutorial volgen

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerScript : MonoBehaviour
-{
 
+{
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
+    {
+        
+    }
+
+
+
+    private void OnEnable()
     {
         
     }
